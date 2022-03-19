0ced7184f6de91ac73dfeb65b1b3a8b6af9bbe2c
Taasin Saqib <taasin.saquib@gmail.com>
2022-03-18 21:26:36 -0800

    final version of thesis

--- writeup/figures/code1.py
+++ writeup/figures/code1.py
@@ -1,5 +1,4 @@
-def forward(self, x)
-
+def forward(self, x):
 	x = self.fc1(x)
 	x = torch.relu(x)
 
@@ -8,15 +7,12 @@ def forward(self, x)
 
 	# Output layer
 	out = self.fc3(x)
-
 	return out
 	
-
-def forward(self, xTensor)
-
+def forward(self, xTensor):
 	# Init LIF Layers to replace Relu
-	mem0 = self.lif0.init_leaky()
-	mem1 = self.lif1.init_leaky()
+	mem0 = self.lif1.init_leaky()
+	mem1 = self.lif2.init_leaky()
 
 	out = None
 
@@ -24,14 +20,13 @@ def forward(self, xTensor)
 		x = xTensor[t]
 
 		x = self.fc1(x)
-		x, mem1 = self.lif0(x, mem1)
+		x, mem1 = self.lif1(x, mem1)
 
 		x = self.fc2(x)
-		x, mem2 = self.lif0(x, mem2)
+		x, mem2 = self.lif2(x, mem2)
 
 		# Output layer
 		out = self.fc3(mem2)
-
 	return out
 
 def forward(self, xTensor)
